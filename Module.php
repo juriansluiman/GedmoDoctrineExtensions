7d6691852b8106b53a4714daad9c577f47fb6c75
Jurian Sluiman <jurian@soflomo.com>
2011-10-25 16:45:12 +0200

    Add classmap to autoload all Gedmo library files

--- Module.php
+++ Module.php
@@ -4,4 +4,17 @@ namespace GedmoDoctrineExtensions;
 
 class Module
 {
+    public function init()
+    {
+        $this->initAutoloader();
+    }
+
+    protected function initAutoloader()
+    {
+        AutoloaderFactory::factory(array(
+            'Zend\Loader\ClassMapAutoloader' => array(
+                __DIR__ . '/autoload_classmap.php',
+            )
+        ));
+    }
 }
